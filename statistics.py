8825dd9ce0667c9acaab54413b548bfc49997307
mp913 <zarkov01@mail.ru>
2020-02-07 12:45:45 +0300

    Initial commit

---
+++ statistics.py
@@ -0,0 +1,6 @@
+
+command = input()
+first_date = input("Enter starting date in")
+first_time = input("Enter starting time")
+second_date = input("Enter starting date")
+second_time = input("Enter starting time")
